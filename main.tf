8e33739867e30b573c131cd881992d5991f7dcad
Leonardo Arroyo <leo_cruz19@hotmail.com>
2026-02-12 20:58:27 -0600

    New terraform files to make cloud agnostic the project

--- main.tf
+++ main.tf
@@ -16,16 +16,15 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = var.aws_region
 }
 
-# 2. Creacion del bucket (Usamos el nombre que ya creaste ayer)
-resource "aws_s3_bucket" "seat_release_bucket" {
-  bucket = "hella-seat-brains-reports-leonardo-arroyo"
+resource "aws_s3_bucket" "reports_bucket" {
+  bucket = var.bucket_name
 
   tags = {
-    Name        = "Seat Brain Reports"
-    Environment = "DevOps training"
+    Name        = "Reports Bucket"
+    Environment = var.environment
   }
 }
 
@@ -37,4 +36,17 @@ resource "aws_s3_bucket_public_access_block" "security_policy" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "cleanup_reports" {
+  bucket = aws_s3_bucket.reports_bucket.id
+
+  rule {
+    id     = "auto-delete-old-reports"
+    status = "Enabled"
+
+    expiration {
+      days = 7
+    }
+  }
 }
