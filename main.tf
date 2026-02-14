95b7b6516b2508bc62d11eb0ff038aefbd68cea6
Leonardo Arroyo <leo_cruz19@hotmail.com>
2026-02-13 22:07:39 -0600

    Resource to create a data base on dynamo is added

--- main.tf
+++ main.tf
@@ -8,11 +8,13 @@ terraform {
   }
 
   # ESTO ES LO QUE CONECTA CON GITHUB ACTIONS
-  backend "s3" {
-    bucket = "hella-seat-brains-reports-leonardo-arroyo"
-    key    = "terraform/state"
-    region = "us-east-2"
-  }
+  #backend "s3" {
+    #bucket         = "hella-seat-brains-reports-leonardo-arroyo"
+    #key            = "terraform.tfstate"
+    #region         = "us-east-2"
+    #dynamodb_table = "terraform-state-locking"
+   # encrypt        = true
+  #}
 }
 
 provider "aws" {
@@ -48,4 +50,15 @@ resource "aws_s3_bucket_public_access_block" "security_policy" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
