36cb290402493c80184981fa74953084f20ea25c
Leonardo Arroyo <leo_cruz19@hotmail.com>
2026-02-15 14:44:22 -0600

    Add IAM security and Lambda function added

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# 1. Definicion del proveedor y Backend
+# 1. Definition of the provider and Backend
 terraform {
   required_providers {
     aws = {
@@ -7,7 +7,7 @@ terraform {
     }
   }
 
-  # ESTO ES LO QUE CONECTA CON GITHUB ACTIONS
+  # This connect to GITHUB Actions
   backend "s3" {
     bucket         = "hella-seat-brains-reports-leonardo-arroyo"
     key            = "terraform.tfstate"
@@ -25,7 +25,7 @@ resource "aws_s3_bucket" "seat_release_bucket" {
   bucket = var.bucket_name
 }
 
-# 2. El Lifecycle
+# 2. Lifecycle
 resource "aws_s3_bucket_lifecycle_configuration" "cleanup_reports" {
   bucket = aws_s3_bucket.seat_release_bucket.id
 
@@ -41,7 +41,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "cleanup_reports" {
   }
 }
 
-# 3. La seguridad 
+# 3. Security
 resource "aws_s3_bucket_public_access_block" "security_policy" {
   bucket = aws_s3_bucket.seat_release_bucket.id
   
@@ -76,5 +76,66 @@ resource "aws_s3_bucket_versioning" "state_versioning" {
   bucket = aws_s3_bucket.seat_release_bucket.id
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+# Automatic ZIP creation
+data "archive_file" "lambda_zip"{
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/lambda_function-zip"
+}
+
+# IAM role to Lambda
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "seat_reports_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Actions = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+#Give permission to write on S3
+resource "aws_iam_role_policy" "lambda_s3_policy" {
+  name = "seat_rports_s3_polic"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:PutObject", "s3:GetObject"]
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.seat_release_bucket.arn}/*"
+      },
+      {
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+# Lambda Function
+resource "aws_lambda_function" "reports_lambda" {
+  filename = data.archive_file.lambda_zip.output_path
+  function_name = "seat-incident-generator"
+  role = aws_iam_role.lambda_exec_role.arn
+  handler = "lambda_function.lambda_handler" # Archivo.funcion
+  runtime = "python3.9"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.seat_release_bucket.id
+    }
   }
 }
---
+++ src/lambda_function.py
@@ -0,0 +1,34 @@
+import boto3
+import panda as pd 
+import os
+from datetime import datetime
+import io
+
+def lambda_handler(event, context):
+    bucket_name = os.environ.get('BUCKET_NAME')
+
+    data = {
+        'Incident_ID': [101, 102, 103],
+        'System': ['Engine', 'Brakes', 'Infotainment'],
+        'Severity': ['High', 'Critical', 'Low'],
+        'Timestamp': [datetime.now().strftime("%Y-%m-%d %H:%M:%S")] * 3
+    }
+
+    df = pd.DataFrame(data)
+
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+
+    s3 = boto3.client('s3')
+    file_name = f"reports/lambda_incident_report_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=file_name
+        Body=csv_buffer.getvalue()
+    )
+
+    return {
+        'statusCode': 200,
+        'body': f"Archivo {file_name} creado exitosamente en {bucket_name}"
+    }
