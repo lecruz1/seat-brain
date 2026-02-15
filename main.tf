61949fa39082c1c49681d85861748d30f3f4a9f9
Leonardo Arroyo <leo_cruz19@hotmail.com>
2026-02-15 14:56:46 -0600

    Delete an extra s on the variable Action

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_iam_role" "lambda_exec_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Actions = "sts:AssumeRole"
+      Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
         Service = "lambda.amazonaws.com"
